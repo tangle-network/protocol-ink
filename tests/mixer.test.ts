d9c7e91ba35fc112378b06760881d140256c690f
oayomide <onigbindeayomide@gmail.com>
2022-02-23 18:34:01 +0100

    another attempt to work with lower level apis

--- tests/mixer.test.ts
+++ tests/mixer.test.ts
@@ -1,7 +1,8 @@
 import { expect } from "chai";
 import { artifacts, network, patract } from "redspot";
 import { CodePromise, ContractPromise, BlueprintPromise, Abi } from "@polkadot/api-contract"
-import fsExtra from 'fs-extra'
+import { u8aToU8a, u8aToHex, u8aConcat, stringToU8a, u8aToString } from '@polkadot/util'
+import { readFile } from 'fs/promises'
 
 
 const { getContractFactory, getRandomSigner } = patract;
@@ -26,32 +27,65 @@ describe('mixer', () => {
 
     it('Creates a new instance of the mixer', async () => {
         try {
-            const { sender, Alice } = await setup();
+            await api.isReady
+            const Alice = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
+            // for now, it'd probably be better to get a random signer but since i am trying
+            // to get this to work, it shoud be fine (and easier to debug) using just 
+            // Alice for now.
+            // const signer = await getRandomSigner(Alice, '100 UNIT')
+            const signerBalance = await api.query.system.account(Alice)
+            // this is correct. checking the substrate-contracts-template frontend, this value is correct
+            console.log(`Alice's balance: ${JSON.stringify((signerBalance.toHuman())?.data?.free, null, 2)}`)
+            // first, set up things
+            const mixerContractAbi = await readFile('./artifacts/mixer.json', { encoding: 'utf8' });
+            const abi = new Abi(mixerContractAbi);
+            const wasm = await readFile('./target/ink/mixer/mixer.wasm');
+            const encodedParams = abi.findConstructor('new').toU8a([30, 100,
+                "0xbd7cd0f84cff12a06a99171c18696cd9ccf598abafcd33682172999221874819",
+                "0x69c3a4cba7f52ba277baeef8fd8315f30a3631984ecb63f59fe861075a0c88d7"])
+
+            const instance = api.tx.contracts.instantiateWithCode(
+                0, // endowment
+                100000, // gas fee
+                0, // storageLimit
+                wasm, // wasm code,
+                encodedParams, // encoded params
+                null // salt
+            )
+            // deploy the code now
+            // const instantiateWithCode = api.
+            console.log('Tx instantiate is', instance.toHuman())
+
+            const tx = instance.withResultTransform(result => result)
+
+
+
+            // const { sender, Alice } = await setup();
 
             // so first we want to create a new code promise. we'll then use the code promise to create a blueprint promise
             // which we'll in-turn use to create the contract promise.
             // however, i am not sure about some of the assumptions in the docs.
-            const mixerCodeInstance = new CodePromise(api, fsExtra.readFileSync('./artifacts/mixer.json', 'utf8'), fsExtra.readFileSync('./target/ink/mixer/mixer.wasm', 'utf8'));
+            // const mixerCodeInstance = new CodePromise(api, fsExtra.readFileSync('./artifacts/mixer.json', 'utf8'), fsExtra.readFileSync('./target/ink/mixer/mixer.wasm', 'utf8')).
 
-            // in order to instantiate a contract, we can do `code.tx.<constructor>(args)`
-            // which is what we have here, however this is still not working as i'd expect
-            // because it returns a promise that resolves to Hash when craeted
+            // // in order to instantiate a contract, we can do `code.tx.<constructor>(args)`
+            // // which is what we have here, however this is still not working as i'd expect
+            // // because it returns a promise that resolves to Hash when craeted
 
-            // perhaps then use the Hash to create a new blueprint, that'll give me the address
-            // of the contract on chain and i can then use that to create a new contract promise
-            const instance = await mixerCodeInstance.tx.new({ gasLimit: 100000 }, 30, 100,
-                "0xbd7cd0f84cff12a06a99171c18696cd9ccf598abafcd33682172999221874819",
-                "0x69c3a4cba7f52ba277baeef8fd8315f30a3631984ecb63f59fe861075a0c88d7").signAndSend(sender.pair);
-            console.log('Code deployment transaction hash is: ', instance.hash.toHuman())
+            // // perhaps then use the Hash to create a new blueprint, that'll give me the address
+            // // of the contract on chain and i can then use that to create a new contract promise
+            // const instance = await mixerCodeInstance.tx.new({ gasLimit: 100000 }, 30, 100,
+            //     "0xbd7cd0f84cff12a06a99171c18696cd9ccf598abafcd33682172999221874819",
+            //     "0x69c3a4cba7f52ba277baeef8fd8315f30a3631984ecb63f59fe861075a0c88d7").signAndSend(sender.pair);
+            // console.log('Code deployment transaction hash is: ', instance.hash.toHuman())
 
-            const bluprint = new BlueprintPromise(api, new Abi(mixerCodeInstance.abi.json), instance.hash);
-            const mixerContract = new ContractPromise(api, bluprint.abi, sender.address);
-            const mixerInitialization = await mixerContract.query.initialize(sender.address, { gasLimit: 100000 });
-            // here, i'd assume that the `initialize` method would be called on the contract and return no errors.
-            // but i still get "contractNotFound" error. Possibly i am using the wrong address but tried every address anyway.
-            console.log(mixerInitialization.result.toHuman())
+            // const bluprint = new BlueprintPromise(api, new Abi(mixerCodeInstance.abi.json), instance.hash);
+            // const mixerContract = new ContractPromise(api, bluprint.abi, sender.address);
+            // const mixerInitialization = await mixerContract.query.initialize(sender.address, { gasLimit: 100000 });
+            // // here, i'd assume that the `initialize` method would be called on the contract and return no errors.
+            // // but i still get "contractNotFound" error. Possibly i am using the wrong address but tried every address anyway.
+            // console.log(mixerInitialization.result.toHuman())
 
-            // const blueprint = new BlueprintPromise(api, fsExtra.readFileSync('./artifacts/mixer.json', 'utf8'), instance.hash);
+            // // const blueprint = new BlueprintPromise(api, fsExtra.readFileSync('./artifacts/mixer.json', 'utf8'), instance.hash);
             // console.log('Blueprint instance tx hash is', blueprint.codeHash.toString())
             // const contract = new ContractPromise(api, new Abi(blueprint.abi.json), blueprint.codeHash.toString());
             // console.log(instance.hash.toHuman())
