243de58875bcc3c8a47de0bd151f345e712a8d7c
Damilare <dharjeezy@gmail.com>
2022-05-12 13:20:15 +0100

    implement merkle tree implement linkable merkle tree

--- contracts/vanchor/lib.rs
+++ contracts/vanchor/lib.rs
@@ -1,17 +1,59 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+mod merkle_tree;
+mod linkable_merkle_tree;
+
 use ink_lang as ink;
 
 #[ink::contract]
 mod vanchor {
+    use linkable_tree::LinkableMerkleTree;
+    use merkle_tree::MerkleTree;
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+
+
+    /// The vanchor result type.
+    pub type Result<T> = core::result::Result<T, Error>;
+
+
     #[ink(storage)]
-    pub struct Vanchor {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+    pub struct VAnchor {
+        /// chain id
+        pub chain_id: u64,
+        /// ERC20 token address
+        pub creator: AccountId,
+        /// The merkle tree
+        pub merkle_tree: MerkleTree,
+        /// The linkable merkle tree
+        pub linkable_tree: LinkableMerkleTree,
+        /// ERC20 token wrapper address
+        pub tokenwrapper_addr: AccountId,
+        /// maximum deposit amount
+        pub max_deposit_amt: Uint128,
+        /// minimum withdrawal amount
+        pub min_withdraw_amt: Uint128,
+        /// maximum externalities amount
+        pub max_ext_amt: Uint128,
+        /// maximum fee
+        pub max_fee: Uint128,
+    }
+
+    /// The vanchor error types.
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        /// Returned if a mapping item is not found
+        ItemNotFound,
+        /// Returned if the merkle tree is full.
+        MerkleTreeIsFull,
+        /// Hash error
+        HashError,
+        /// Verify error
+        VerifyError,
+        /// Edege already exists
+        EdgeAlreadyExists,
+        ///  Too many edges
+        TooManyEdges,
     }
 
     impl Vanchor {
@@ -44,31 +86,4 @@ mod vanchor {
         }
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// Imports `ink_lang` so we can use `#[ink::test]`.
-        use ink_lang as ink;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let vanchor = Vanchor::default();
-            assert_eq!(vanchor.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut vanchor = Vanchor::new(false);
-            assert_eq!(vanchor.get(), false);
-            vanchor.flip();
-            assert_eq!(vanchor.get(), true);
-        }
-    }
 }
---
+++ contracts/vanchor/merkle_tree.rs
@@ -0,0 +1,88 @@
+use ink_storage::Mapping;
+use ink_storage::traits::{SpreadLayout, SpreadAllocate};
+#[cfg(feature = "std")]
+use ink_storage::traits::StorageLayout;
+
+use ink_prelude::vec;
+use crate::vanchor;
+
+pub const ROOT_HISTORY_SIZE: u32 = 100;
+
+#[derive(Default, Debug, SpreadLayout, SpreadAllocate)]
+#[cfg_attr(feature = "std", derive(StorageLayout))]
+pub struct MerkleTree {
+    pub levels: u32,
+    pub current_root_index: u32,
+    pub next_index: u32,
+    pub filled_subtrees: Mapping<u32, [u8; 32]>,
+    pub roots: Mapping<u32, [u8; 32]>,
+}
+
+impl MerkleTree {
+    fn hash_left_right(
+        &self,
+        hasher: PoseidonRef,
+        left: [u8; 32],
+        right: [u8; 32],
+    ) -> Result<[u8; 32]> {
+        let inputs = vec![left, right];
+        hasher.hash(inputs).map_err(|_| vanchor::Error::HashError)
+    }
+
+    pub fn insert(&mut self, hasher: PoseidonRef, leaf: [u8; 32]) -> Result<u32> {
+        let next_index = self.next_index;
+        assert!(
+            !next_index == u32::from(2u32.pow(self.levels as u32)),
+            "Merkle tree is full"
+        );
+
+        let mut current_index = next_index;
+        let mut current_level_hash = leaf;
+        let mut left: [u8; 32];
+        let mut right: [u8; 32];
+
+        for i in 0..self.levels {
+            if current_index % 2 == 0 {
+                left = current_level_hash;
+                right = zeroes::zeroes(i);
+                self.filled_subtrees.insert(i, &current_level_hash);
+            } else {
+                left = self.filled_subtrees.get(&i).unwrap_or_default();
+                right = current_level_hash;
+            }
+
+            current_level_hash = self.hash_left_right(hasher.clone(), left, right)?;
+            current_index = current_index / 2;
+        }
+
+        let new_root_index = (self.current_root_index + 1) % ROOT_HISTORY_SIZE;
+        self.current_root_index = new_root_index;
+        self.roots.insert(new_root_index, &current_level_hash);
+        self.next_index = next_index + 1;
+        Ok(next_index)
+    }
+
+    pub fn is_known_root(&self, root: [u8; 32]) -> bool {
+        if root == [0u8; 32] {
+            return false;
+        }
+
+        let mut i = self.current_root_index;
+        for _ in 0..ROOT_HISTORY_SIZE {
+            if let Some(r) = self.roots.get(&i) {
+                if r == root {
+                    return true;
+                }
+
+                if i == 0 {
+                    i = ROOT_HISTORY_SIZE - 1;
+                } else {
+                    i -= 1;
+                }
+            }
+        }
+
+        false
+    }
+}
+
