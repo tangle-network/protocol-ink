d7b91769536189545b62e617f76c115a281847e4
Drew Stone <drewstone329@gmail.com>
2022-05-20 13:50:40 -0400

    Fix comment

--- contracts/vanchor/lib.rs
+++ contracts/vanchor/lib.rs
@@ -58,7 +58,7 @@ mod vanchor {
         HashError,
         /// Verify error
         VerifyError,
-        /// Edege already exists
+        /// Edge already exists
         EdgeAlreadyExists,
         ///  Too many edges
         TooManyEdges,
