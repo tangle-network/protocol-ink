17cd11fc1f6613b00777be54f992b0ff41893b94
Drew Stone <drewstone329@gmail.com>
2022-06-15 11:22:50 -0400

    Update test with proper input

--- tests/mixer.test.ts
+++ tests/mixer.test.ts
@@ -1,54 +1,64 @@
-import { assert, expect } from "chai";
-import { artifacts, network, patract } from "redspot";
+import { assert, expect } from 'chai';
+import { artifacts, network, patract } from 'redspot';
 
 import {
   generate_proof_js,
   JsNote,
   JsNoteBuilder,
-} from "@webb-tools/wasm-utils/njs";
-import BN from "bn.js";
-import {ArkworksProvingManager, MerkleTree, Note, NoteGenInput, ProvingManagerSetupInput} from "@webb-tools/sdk-core";
+  verify_js_proof,
+} from '@webb-tools/wasm-utils/njs/wasm-utils-njs.js';
+import BN from 'bn.js';
+import {ArkworksProvingManager, MerkleTree, Note, NoteGenInput, ProvingManagerSetupInput} from '@webb-tools/sdk-core';
 
 const { getContractFactory, getRandomSigner } = patract;
 const { api, getAddresses, getSigners } = network;
-import { u8aToHex, hexToU8a } from "@polkadot/util";
-import path from "path";
+import { u8aToHex, hexToU8a } from '@polkadot/util';
+import path from 'path';
 import fs from "fs";
 import { decodeAddress } from "@polkadot/util-crypto";
-import child from "child_process";
+import child from 'child_process';
+
+async function fetchSubstrateMixerProvingKey() {
+  const IPFSUrl = 'https://ipfs.io/ipfs/QmfQUgqRXCdUiogiRU8ZdLFZD2vqVb9fHpLkL6DsGHwoLH';
+  const ipfsKeyRequest = await fetch(IPFSUrl);
+  const circuitKeyArrayBuffer = await ipfsKeyRequest.arrayBuffer();
+  const circuitKey = new Uint8Array(circuitKeyArrayBuffer);
+
+  return circuitKey;
+}
 
 export function generateDeposit(amount: number) {
   let noteBuilder = new JsNoteBuilder();
-  noteBuilder.protocol("mixer");
-  noteBuilder.version("v2");
+  noteBuilder.protocol('mixer');
+  noteBuilder.version('v2');
 
-  noteBuilder.sourceChainId("1");
-  noteBuilder.targetChainId("1");
+  noteBuilder.sourceChainId('1');
+  noteBuilder.targetChainId('1');
 
-  noteBuilder.tokenSymbol("WEBB");
-  noteBuilder.amount("1");
-  noteBuilder.sourceIdentifyingData("3");
-  noteBuilder.targetIdentifyingData("3");
-  noteBuilder.denomination("18");
+  noteBuilder.tokenSymbol('WEBB');
+  noteBuilder.amount('1');
+  noteBuilder.sourceIdentifyingData('3');
+  noteBuilder.targetIdentifyingData('3');
+  noteBuilder.denomination('18');
 
-  noteBuilder.backend("Arkworks");
-  noteBuilder.hashFunction("Poseidon");
-  noteBuilder.curve("Bn254");
-  noteBuilder.width("3");
-  noteBuilder.exponentiation("5");
+  noteBuilder.backend('Arkworks');
+  noteBuilder.hashFunction('Poseidon');
+  noteBuilder.curve('Bn254');
+  noteBuilder.width('3');
+  noteBuilder.exponentiation('5');
   const note = noteBuilder.build();
 
   return note;
 }
 
-// to call a "method", you use contract.tx.methodName(args). to get a value, you use contract.query.methodName(args).
-describe("mixer", () => {
+// to call a 'method', you use contract.tx.methodName(args). to get a value, you use contract.query.methodName(args).
+describe('mixer', () => {
   after(() => {
     return api.disconnect();
   });
 
   async function setup() {
-    console.log("trying setup");
+    console.log('trying setup');
     await api.isReady;
     const one = new BN(10).pow(new BN(api.registry.chainDecimals[0]));
     const signerAddresses = await getAddresses();
@@ -57,33 +67,33 @@ describe("mixer", () => {
     const BobSigner = await getRandomSigner(Alice, one.muln(10));
     const sender = await getRandomSigner(Alice, one.muln(10));
 
-    console.log("finished trying set up");
+    console.log('finished trying set up');
     return { sender, Alice, BobSigner };
   }
 
-  it.only("Creates a new instance of the mixer", async () => {
+  it.only('Creates a new instance of the mixer', async () => {
     const { sender, BobSigner } = await setup();
 
-    console.log("creating poseidon contract");
+    console.log('creating poseidon contract');
     // Poseidon instantiation
     const poseidonContractFactory = await getContractFactory(
-      "poseidon",
+      'poseidon',
       sender.address
     );
-    const poseidonContract = await poseidonContractFactory.deploy("new");
+    const poseidonContract = await poseidonContractFactory.deploy('new');
 
-    console.log("poseidon deployed");
+    console.log('poseidon deployed');
 
     // Mixer verifier instantiation
     const mixerVerifierContractFactory = await getContractFactory(
-      "mixer_verifier",
+      'mixer_verifier',
       sender.address
     );
     const mixerVerifierContract = await mixerVerifierContractFactory.deploy(
-      "new"
+      'new'
     );
 
-    console.log("mixer verifier deployed");
+    console.log('mixer verifier deployed');
 
     // console.log(poseidonContract.abi.info.source.wasmHash);
     // console.log(mixerVerifierContract.abi.info.source.wasmHash);
@@ -93,11 +103,11 @@ describe("mixer", () => {
     const levels = 30;
     const depositSize = 100;
     const mixerContractFactory = await getContractFactory(
-      "mixer",
+      'mixer',
       sender.address
     );
     const mixerContract = await mixerContractFactory.deploy(
-      "new",
+      'new',
       levels,
       depositSize,
       randomVersion,
@@ -136,7 +146,7 @@ describe("mixer", () => {
     // the secrets which were used in generating a leaf, without revealing the secrets.
     const commitment = note.getLeaf();
 
-    console.log("sending deposit");
+    console.log('sending deposit');
     const depositFunction = await mixerContract.tx.deposit(commitment, {
       value: depositSize,
     });
@@ -146,33 +156,32 @@ describe("mixer", () => {
     const pm = new ArkworksProvingManager(undefined);
 
     const gitRoot = child
-      .execSync("git rev-parse --show-toplevel")
+      .execSync('git rev-parse --show-toplevel')
       .toString()
       .trim();
     const provingKeyPath = path.join(
       gitRoot,
-      "tests",
-      "protocol-substrate-fixtures",
-      "mixer",
-      "bn254",
-      "x5",
-      "proving_key_uncompressed.bin"
+      'tests',
+      'protocol-substrate-fixtures',
+      'mixer',
+      'bn254',
+      'x5',
+      'proving_key_uncompressed.bin'
     );
     const provingKey = fs.readFileSync(provingKeyPath);
 
     const accountId = sender.address;
     const addressHex = u8aToHex(decodeAddress(accountId));
-    const relayerAddressHex = u8aToHex(decodeAddress(accountId));
 
-    const provingInput: ProvingManagerSetupInput<"mixer"> = {
+    const provingInput: ProvingManagerSetupInput<'mixer'> = {
       leafIndex: 0,
-      provingKey: hexToU8a(provingKey.toString("hex")),
+      provingKey: hexToU8a(provingKey.toString('hex')),
       note: note.serialize(),
       fee: 0,
       refund: 0,
       leaves: [commitment],
-      recipient: addressHex.replace("0x", ""),
-      relayer: relayerAddressHex.replace("0x", ""),
+      recipient: addressHex.replace('0x', ''),
+      relayer: addressHex.replace('0x', ''),
     };
 
     console.log(`provingInput is ${provingInput}`);
@@ -182,15 +191,14 @@ describe("mixer", () => {
     let root = `0x${proof.root}`;
     let nullifier_hash = `0x${proof.nullifierHash}`;
     let recipient = sender.address;
-    let relayer =  BobSigner.address;
+    let relayer =  sender.address;
     let fee = 0;
     let refund = 0;
 
+    // const isValidProof = verify_js_proof(proof, publicInputs, u8aToHex(keys.vk).replace('0x', ''), 'Bn254');
 
-    console.log("sending withdrawal");
+    console.log('sending withdrawal');
     const withdrawFunction = await mixerContract.tx.withdraw(proof_bytes, root, nullifier_hash, recipient, relayer, fee, refund);
     expect(withdrawFunction).to.be.ok;
-
-
   });
 });
