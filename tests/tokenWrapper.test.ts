c5dd8ff29bbc0bed3bb792b90fb0f0aa426c4fee
Damilare <dharjeezy@gmail.com>
2022-06-17 18:23:22 +0100

    console

--- tests/tokenWrapper.test.ts
+++ tests/tokenWrapper.test.ts
@@ -42,13 +42,15 @@ describe("token-wrapper", () => {
     const startArgs: string[] = [];
     startArgs.push("--tmp -lruntime=debug")
     const ls = spawn( './artifacts/substrate-contracts-node-linux/substrate-contracts-node',
-    startArgs);
+    []);
 
     ls.stdout.on('data', (data) => {
+      console.log(`printing out data`);
       console.log(`stdout: ${data}`);
     });
 
     ls.stderr.on('data', (data) => {
+      console.log(`printing out error`);
       console.error(`stderr: ${data}`);
     });
 
