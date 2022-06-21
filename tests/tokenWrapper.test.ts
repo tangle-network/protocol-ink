7e7ab3bed3b2a03160a856992e9db34f39d52601
Damilare <dharjeezy@gmail.com>
2022-06-21 08:54:29 +0100

    clean version of CI integration

--- tests/tokenWrapper.test.ts
+++ tests/tokenWrapper.test.ts
@@ -3,6 +3,7 @@ import { artifacts, network, patract } from "redspot";
 import BN from "bn.js";
 import { all } from "@polkadot/api-derive/balances";
 import { hexToU8a } from "@polkadot/util";
+import { startContractNode } from "./util";
 
 const { getContractFactory, getRandomSigner } = patract;
 const { api, getAddresses, getSigners } = network;
@@ -36,6 +37,7 @@ describe("token-wrapper", () => {
   });
 
   before(async () => {
+    await startContractNode();
     await api.isReady;
   });
 
